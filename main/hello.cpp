f7fa49df7178ff67f73c2ab0618eb6480bdf2909
KSRAO-2002 <79738613+KSRAO-2002@users.noreply.github.com>
2023-02-18 22:31:55 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 
 int main(void) {
+  cout << "Hey! I am Sujan Rao R\n";
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "Hey! I am Sujan Rao R";
   return 0;
 }
